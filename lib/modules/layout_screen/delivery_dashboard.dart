f985037cd350277ff36c785e58f5c713e85bc3df
Vijay-Mahato <hellodevvijay2003@gmail.com>
2024-11-20 12:35:05 +0530

    order dashboard

--- lib/modules/layout_screen/delivery_dashboard.dart
+++ lib/modules/layout_screen/delivery_dashboard.dart
@@ -1,56 +1,297 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../order_models/new_order_models.dart';
+import '../../orders_api/get_new_orders.dart';
+import '../../widgets/orders_screen/order_button.dart';
 
-import '../../components/order_button.dart';
-import '../../components/stats_card.dart';
-import '../../components/status_button.dart';
+class DeliveryDashboard extends StatefulWidget {
+  const DeliveryDashboard({super.key});
 
-class DeliveryDashboard extends StatelessWidget {
+  @override
+  State<DeliveryDashboard> createState() => _DeliveryDashboardState();
+}
+
+class _DeliveryDashboardState extends State<DeliveryDashboard> {
+  String selectedButton = 'New Order';
+  List<NewOrderModels> orderItems = [];
+
+  @override
+  void initState() {
+    getOrders();
+    super.initState();
+  }
+
+  getOrders() async {
+    final tempOrder = await fetchorders();
+    setState(() {
+      orderItems = tempOrder;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          'Odoo Delivery Boy',
-          style: TextStyle(color: Colors.black),
-        ),
-        elevation: 0,
+        // title: const Text(
+        //   'Delivery Dashboard',
+        //   style: TextStyle(
+        //     fontWeight: FontWeight.bold,
+        //     color: Colors.white,
+        //   ),
+        // ),
+        // backgroundColor: const Color(0xFF0044B4),
+        // elevation: 10,
+        // centerTitle: true,
       ),
-      drawer: Drawer(), // Add drawer if needed
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            OrderButton(
-              title: 'Accept orders',
-              icon: Icons.list,
-              onTap: () {
-                // Handle accept orders action
-              },
+      body: Column(
+        children: [
+          // Navigation Buttons Section
+          Container(
+            height: 60,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF5F5F5),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 8,
+                  offset: Offset(0, 4),
+                ),
+              ],
             ),
-            OrderButton(
-              title: 'Pending orders',
-              icon: Icons.access_time,
-              onTap: () {
-                // Handle pending orders action
-              },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                OrderButtons(
+                  buttonName: "New Order",
+                  selectedButton: selectedButton,
+                  onSelected: (name) {
+                    setState(() {
+                      selectedButton = name;
+                    });
+                  },
+                ),
+                OrderButtons(
+                  buttonName: "On Going",
+                  selectedButton: selectedButton,
+                  onSelected: (name) {
+                    setState(() {
+                      selectedButton = name;
+                    });
+                  },
+                ),
+                OrderButtons(
+                  buttonName: "Completed",
+                  selectedButton: selectedButton,
+                  onSelected: (name) {
+                    setState(() {
+                      selectedButton = name;
+                    });
+                  },
+                ),
+              ],
             ),
-            OrderButton(
-              title: 'Completed orders',
-              icon: Icons.check_circle,
-              onTap: () {
-                // Handle completed orders action
-              },
-            ),
-            SizedBox(height: 20),
-            StatsCard(),
-            Spacer(),
-            StatusButton(),
-          ],
-        ),
+          ),
+          orderItems.isEmpty
+              ? const Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: Color(0xFF0044B4),
+                      strokeWidth: 3,
+                    ),
+                  ),
+                )
+              : Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: ListView.builder(
+                      itemCount: orderItems.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          elevation: 5,
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "OrderId: ${orderItems[index].orderId}",
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 5),
+                                      decoration: BoxDecoration(
+                                        color: Colors.red,
+                                        borderRadius: BorderRadius.circular(12),
+                                      ),
+                                      child: const Text(
+                                        'New Order',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    // Food Image Section
+                                    Container(
+                                      height: 90,
+                                      width: 90,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        border: Border.all(
+                                            width: 2,
+                                            color: Colors.grey.shade300),
+                                      ),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: Image.network(
+                                          orderItems[index].image,
+                                          fit: BoxFit.cover,
+                                          width: 90,
+                                          height: 90,
+                                          loadingBuilder: (context, child,
+                                              loadingProgress) {
+                                            if (loadingProgress == null) {
+                                              return child;
+                                            } else {
+                                              return const Center(
+                                                child:
+                                                    CircularProgressIndicator(
+                                                  color: Color(0xFF0044B4),
+                                                  strokeWidth: 3,
+                                                ),
+                                              );
+                                            }
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 12),
+                                    // Order Details Section
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            orderItems[index].hotelName,
+                                            style: const TextStyle(
+                                              color: Color(0xFF1F3D7B),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 5),
+                                          Text(
+                                            orderItems[index].foodName,
+                                            style: const TextStyle(
+                                              color: Color(0xFF9B4416),
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 5),
+                                          Text(
+                                            orderItems[index].address,
+                                            style: TextStyle(
+                                              color:
+                                                  Colors.black.withOpacity(0.7),
+                                              fontWeight: FontWeight.w300,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                          const SizedBox(height: 5),
+                                          Text(
+                                            "₹${orderItems[index].price.toString()}",
+                                            style: const TextStyle(
+                                              color: Color.fromARGB(
+                                                  255, 6, 116, 11),
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 15,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Divider(),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    TextButton(
+                                      onPressed: () {
+                                        // Add Order Details logic here
+                                      },
+                                      style: TextButton.styleFrom(
+                                        backgroundColor:
+                                            const Color(0xFF068C16),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                      ),
+                                      child: const Text(
+                                        'Order Details',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        // Add Customer Information logic here
+                                      },
+                                      style: TextButton.styleFrom(
+                                        backgroundColor:
+                                            const Color(0xFF068C16),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                      ),
+                                      child: const Text(
+                                        'Customer Information',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+        ],
       ),
     );
   }
